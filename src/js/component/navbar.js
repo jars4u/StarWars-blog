7eba03bb027b1e60eedc0bbcc3bec4faa12192c9
Juan Ramos <127767142+jars4u@users.noreply.github.com>
2023-08-08 21:18:55 +0000

    favorite btn at navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,16 +4,19 @@ import "../../styles/index.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light m-3">
+		<nav className="navbar navbar-light bg-light mx-5">
 			<Link to="/">
 				<img
 					src="https://www.nicelembrancinhas.com.br/image/cache/catalog/STAR%20WARS/LOGO%20STAR%20WARS%205CM-650x650.png"
 					className="logo" alt="logo-img" />
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Favorites</button>
-				</Link>
+			<div className="btn-group">
+				<button className="btn btn-secondary btn-lg dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+					Favorites
+				</button>
+				<ul className="dropdown-menu">
+					...
+				</ul>
 			</div>
 		</nav>
 	);

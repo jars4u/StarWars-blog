64d066aa140a935535f8d7673808b83b468c4dd5
Juan Ramos <127767142+jars4u@users.noreply.github.com>
2023-08-08 20:25:54 +0000

    def store and home

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,9 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useEffect, useContext, useState } from "react";
+import { Context } from "../store/appContext.js";
+import { Card } from "../component/card.js";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const { store, actions } = useContext(Context)
+	const { people, vehicles, planets} = store
+};
